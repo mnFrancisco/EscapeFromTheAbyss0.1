e8b131b96db12e1813d5228b9707b0e67a566a43
darckrebon <92961019+mnFrancisco@users.noreply.github.com>
2023-09-15 00:47:46 -0300

    vc

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,34 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
+
 public class PlayerMovement : MonoBehaviour
 {
+    private Animator animator;
+
     private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 16f;
+    public int speed = 8;
+    public int jumpingPower = 16;
     private bool isFacingRight = true;
  
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+
+    void Start(){
+
+        animator = GetComponent<Animator>() ;
+
+    }
  
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
- 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        
+        // Detecta se o jogador está se movendo horizontalmente.
+        bool isMoving = Mathf.Abs(horizontal) > 0;
+
+        // Define o parâmetro "andando" no Animator com base no movimento.
+        animator.SetBool("andando", isMoving);
+
+        // Verifica se o jogador está no chão.
+        bool isGrounded = IsGrounded();
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
- 
+
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
- 
+
         Flip();
     }
+
  
     private void FixedUpdate()
     {
